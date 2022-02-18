e3062a48eebc1bcaf97f48c2f2b0093a10f9beb8
Spoorthi M <spoorthi2112@gmail.com>
2022-02-18 21:06:47 +0530

    modified drone utils to return results as strings for flask

--- drone_db_ops.py
+++ drone_db_ops.py
@@ -10,32 +10,41 @@ class droneDB():
         self.cursor = self.db.cursor()
 
     def get_db_details(self):
-        print('DATABASE DETAILS')
-        print(f'Database tech: mysql')
-        print(f'Database name: {self.database}')
+        res_str = ''
+        res_str += f'DATABASE DETAILS\n'
+        res_str += f'Database tech: mysql\n'
+        res_str += f'Database name: {self.database}\n'
         try:
             self.cursor.execute('show tables;')
         except:
-            print('Exception in showing tables!')
-            return
+            res_str += f'Exception in showing tables!'
+            return res_str
         result = self.cursor.fetchall()
+        res_str += 'The list of tables:\n'
+        res_str += '\n'.join([str(item) for item in result])
+        return res_str
+        '''
         print('Showing list of tables...')
         for x in result:
             print(x)
         print('DONE')
+        '''
 
     def desc_table(self, tablename):
         query = f'desc {tablename}'
         try:
             self.cursor.execute(query)
         except:
-            print('ERROR!')
-            return
+            return f'ERROR in fetching description of {tablename}'
         result = self.cursor.fetchall()
-        print(f'Showing description of table {tablename}')
+        res_str = f'Showing description of table {tablename}'
+        res_str += '\n'.join([str(item) for item in result])
+        '''
         for x in result:
             print(x)
-        print('DONE')
+        '''
+        return res_str
+
 
     def insert_records(self, tablename, val_tuple):
         query_str = f'INSERT INTO {tablename} VALUES '
@@ -48,21 +57,25 @@ class droneDB():
             self.cursor.execute(query_str)
             self.db.commit()
         except:
-            print('Exception in inserting!!')
             self.db.rollback()
-            return
-        print('Insertion operation complete')
+            return f'Exception in inserting into table {tablename}!!'
+        return f'Insertion operation complete into table {tablename}'
 
     def display_entries(self, tablename):
         query_str = f'SELECT * FROM {tablename}'
         try:
             self.cursor.execute(query_str);
         except:
-            print('Exception in displaying!')
+            return f'Exception in displaying table {tablename}!'
         result = self.cursor.fetchall()
+        res_str = f'Displaying entries of table {tablename}\n'
+        res_str += '\n'.join(str(item) for item in result)
+        '''
         for x in result:
             print(x)
         print('Displaying DONE')
+        '''
+        return res_str
 
     def delete_rows(self, tablename, condition):
         query_str = f'DELETE FROM {tablename} WHERE {condition}'
@@ -70,27 +83,23 @@ class droneDB():
             self.cursor.execute(query_str)
             self.db.commit()
         except:
-            print('Exception in deletion!')
             self.db.rollback()
-            return
-        print('Deletion DONE')
+            return f'Exception in deletion from table {tablename}!'
+        return f'Deletion DONE from table {tablename}'
 
 #FOR TESTING
 '''
 drone_db = droneDB('localhost', 'root', 'Unbxd@123', 'drone_info')
-
 print('TABLE DESCRIPTIONS')
-drone_db.desc_table('country')
-drone_db.desc_table('company')
-
+print(drone_db.desc_table('country'))
+print(drone_db.desc_table('company'))
 print('DISPLAY ALL ENTRIES OF A TABLE')
-drone_db.display_entries('security_features')
-
+print(drone_db.display_entries('security_features'))
 print('INSERTIONS AND DELETION')
 val_tuple = ((22, 'abc'), (33, 'xyz'))
-drone_db.insert_records('country', val_tuple)
-drone_db.delete_rows('country', 'country_ID=22')
-drone_db.delete_rows('country', 'country_ID=33')
-
-drone_db.get_db_details()
+print(drone_db.insert_records('country', val_tuple))
+print(drone_db.delete_rows('country', 'country_ID=22'))
+print(drone_db.delete_rows('country', 'country_ID=33'))
+print(drone_db.get_db_details())
+print(drone_db.display_entries('security_features'))
 '''
