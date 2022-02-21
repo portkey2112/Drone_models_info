6fd1184b96ff2e2ff5c60d4ef77c700494f67880
Spoorthi M <spoorthi2112@gmail.com>
2022-02-21 16:18:09 +0530

    fix (db): Seperate insert functions for seperate tables
    This is done to have proper URL paramters for flask URL.Having
    one insert meant passing all parameters as one string in the
    URL and special characters need to be handled (like quotes).
    So, seperated inserts. Now, each vol value for a table can
    be specified as a seperate value in the URL.
    Ex: /insert_into_table/<col1_val>/<col2_val>/...
    
    Flask script needs to be modified. Need to add different URLs.`

--- drone_db_ops.py
+++ drone_db_ops.py
@@ -45,21 +45,118 @@ class droneDB():
         '''
         return res_str
 
-
-    def insert_records(self, tablename, val_tuple):
-        query_str = f'INSERT INTO {tablename} VALUES '
-        for i in range(len(val_tuple)):
-            if(i == 0):
-                query_str += str(val_tuple[i])
-            else:
-                query_str += ', ' + str(val_tuple[i])
+    def insert_into_automation_features(self, arg_auto_feat_ID, arg_name, arg_description):
+        query_str = f'INSERT INTO automation_features VALUES ({arg_auto_feat_ID}, \'{arg_name}\', \'{arg_description}\')'
+        print(query_str)
         try:
             self.cursor.execute(query_str)
             self.db.commit()
-        except:
+        except Exception as e:
             self.db.rollback()
-            return f'Exception in inserting into table {tablename}!!'
-        return f'Insertion operation complete into table {tablename}'
+            return f'Exception in inserting into table automation_features !!, {str(e)}'
+        return f'Insertion operation complete into table automation_features'
+
+    def insert_into_company(self, company_id, name):
+        query_str = f'INSERT INTO company VALUES ({company_id}, \'{name}\')'
+        try:
+            self.cursor.execute(query_str)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            return f'Exception in inserting into table company !!, {str(e)}'
+        return f'Insertion operation complete into table company'
+
+    def insert_into_country(self, country_id, name):
+        query_str = f'INSERT INTO country VALUES ({country_id}, \'{name}\')'
+        try:
+            self.cursor.execute(query_str)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            return f'Exception in inserting into table country !!, {str(e)}'
+        return f'Insertion operation complete into table country'
+
+    def insert_into_cv_algorithm(self, algo_ID, name):
+        query_str = f'INSERT INTO cv_algorithm VALUES ({algo_ID}, \'{name}\')'
+        try:
+            self.cursor.execute(query_str)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            return f'Exception in inserting into table cv_algorithm !!, {str(e)}'
+        return f'Insertion operation complete into table cv_algorithm'
+
+    def insert_into_domain(self, domain_ID, name):
+        query_str = f'INSERT INTO domain VALUES ({domain_ID}, \'{name}\')'
+        try:
+            self.cursor.execute(query_str)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            return f'Exception in inserting into table domain !!, {str(e)}'
+        return f'Insertion operation complete into table domain'
+
+    def insert_into_drone_models(self, ID, Name, company_id, height,
+    users, licence, training, price, weight_in_kg, country_of_operation_ID,
+    domain_ID, usecase, wingspan):
+        query_str = f'INSERT INTO drone_models VALUES ({ID}, \'{Name}\', {company_id}, {height}, \'{users}\', {licence}, {training}, {price}, {weight_in_kg}, {country_of_operation_ID}, {domain_ID},\'{usecase}\', {wingspan})'
+        try:
+            self.cursor.execute(query_str)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            return f'Exception in inserting into table drone_models !!, {str(e)}'
+        return f'Insertion operation complete into table drone_models'
+
+    def insert_into_pair_drone_automation_feature(self, drone_ID, auto_feat_ID):
+        query_str = f'INSERT INTO pair_drone_automation_feature VALUES ({drone_ID}, {auto_feat_ID})'
+        try:
+            self.cursor.execute(query_str)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            return f'Exception in inserting into table pair_drone_automation_feature  !!, {str(e)}'
+        return f'Insertion operation complete into table pair_drone_automation_feature'
+
+    def insert_into_pair_drone_country(self, drone_ID, country_id):
+        query_str = f'INSERT INTO pair_drone_country VALUES ({drone_ID}, {country_id})'
+        try:
+            self.cursor.execute(query_str)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            return f'Exception in inserting into table pair_drone_country  !!, {str(e)}'
+        return f'Insertion operation complete into table pair_drone_country'
+
+    def insert_into_pair_drone_cv_algo(self, drone_ID, cv_algo_ID):
+        query_str = f'INSERT INTO pair_drone_cv_algo VALUES ({drone_ID}, {cv_algo_ID})'
+        try:
+            self.cursor.execute(query_str)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            return f'Exception in inserting into table pair_drone_cv_algo  !!, {str(e)}'
+        return f'Insertion operation complete into table pair_drone_cv_algo'
+
+    def insert_into_pair_drone_security_feature(self, drone_ID, sec_feat_ID):
+        query_str = f'INSERT INTO pair_drone_security_feature VALUES ({drone_ID}, {sec_feat_ID})'
+        try:
+            self.cursor.execute(query_str)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            return f'Exception in inserting into table pair_drone_security_feature  !!, {str(e)}'
+        return f'Insertion operation complete into table pair_drone_security_feature'
+
+    def insert_into_security_features(self, sec_feat_ID, name, decription):
+        query_str = f'INSERT INTO security_features VALUES ({sec_feat_ID}, \'{name}\', \'{decription}\')'
+        try:
+            self.cursor.execute(query_str)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            return f'Exception in inserting into table security_features  !!, {str(e)}'
+        return f'Insertion operation complete into table security_features'
 
     def display_entries(self, tablename):
         query_str = f'SELECT * FROM {tablename}'
@@ -82,14 +179,16 @@ class droneDB():
         try:
             self.cursor.execute(query_str)
             self.db.commit()
-        except:
+        except Exception as e:
             self.db.rollback()
-            return f'Exception in deletion from table {tablename}!'
+            return f'Exception in deletion from table {tablename}!, {str(e)}'
         return f'Deletion DONE from table {tablename}'
 
 #FOR TESTING
 '''
 drone_db = droneDB('localhost', 'root', 'Unbxd@123', 'drone_info')
+print(drone_db. insert_into_drone_models(111, 'dss2', 1, 1, 1, 0,0,0,0,1,1,'dd',0))
+print(drone_db.delete_rows('drone_models', 'ID=111'))
 print('TABLE DESCRIPTIONS')
 print(drone_db.desc_table('country'))
 print(drone_db.desc_table('company'))
